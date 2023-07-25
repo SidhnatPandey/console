8ab97cecd629437efae24e0e4fb4e4c77af8668b
ShivaniShekhawat26 <136352208+ShivaniShekhawat26@users.noreply.github.com>
2023-07-25 13:58:16 +0530

    INIT-13:Registration Page UI Code with API Integration *Implemented Validation *Submit Handling *Routing after submit

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,107 +1,148 @@
 // ** React Imports
-import { ReactNode, useState } from 'react'
+import { ReactNode, useState } from "react";
+
+//**the useRouter hook */
+import { useRouter } from "next/router";
 
 // ** Next Import
-import Link from 'next/link'
+import Link from "next/link";
 
 // ** MUI Components
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
-import Checkbox from '@mui/material/Checkbox'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import Box, { BoxProps } from '@mui/material/Box'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { styled, useTheme } from '@mui/material/styles'
-import InputAdornment from '@mui/material/InputAdornment'
-import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Box, { BoxProps } from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { styled, useTheme } from "@mui/material/styles";
+import InputAdornment from "@mui/material/InputAdornment";
+import MuiFormControlLabel, {
+  FormControlLabelProps,
+} from "@mui/material/FormControlLabel";
 
 // ** Custom Component Import
-import CustomTextField from 'src/@core/components/mui/text-field'
+import CustomTextField from "src/@core/components/mui/text-field";
 
 // ** Icon Imports
-import Icon from 'src/@core/components/icon'
+import Icon from "src/@core/components/icon";
 
 // ** Layout Import
-import BlankLayout from 'src/@core/layouts/BlankLayout'
+import BlankLayout from "src/@core/layouts/BlankLayout";
 
 // ** Hooks
-import { useSettings } from 'src/@core/hooks/useSettings'
+import { useSettings } from "src/@core/hooks/useSettings";
 
 // ** Demo Imports
-import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
+import FooterIllustrationsV2 from "src/views/pages/auth/FooterIllustrationsV2";
 
 // ** Styled Components
-const RegisterIllustration = styled('img')(({ theme }) => ({
+const RegisterIllustration = styled("img")(({ theme }) => ({
   zIndex: 2,
   maxHeight: 600,
   marginTop: theme.spacing(12),
   marginBottom: theme.spacing(12),
   [theme.breakpoints.down(1540)]: {
-    maxHeight: 550
+    maxHeight: 550,
   },
-  [theme.breakpoints.down('lg')]: {
-    maxHeight: 500
-  }
-}))
+  [theme.breakpoints.down("lg")]: {
+    maxHeight: 500,
+  },
+}));
 
 const RightWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  width: '100%',
-  [theme.breakpoints.up('md')]: {
-    maxWidth: 450
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    maxWidth: 450,
   },
-  [theme.breakpoints.up('lg')]: {
-    maxWidth: 600
+  [theme.breakpoints.up("lg")]: {
+    maxWidth: 600,
   },
-  [theme.breakpoints.up('xl')]: {
-    maxWidth: 750
-  }
-}))
+  [theme.breakpoints.up("xl")]: {
+    maxWidth: 750,
+  },
+}));
 
 const LinkStyled = styled(Link)(({ theme }) => ({
-  textDecoration: 'none',
-  color: `${theme.palette.primary.main} !important`
-}))
+  textDecoration: "none",
+  color: `${theme.palette.primary.main} !important`,
+}));
 
-const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ theme }) => ({
-  marginTop: theme.spacing(1.5),
-  marginBottom: theme.spacing(1.75),
-  '& .MuiFormControlLabel-label': {
-    color: theme.palette.text.secondary
-  }
-}))
+const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(
+  ({ theme }) => ({
+    marginTop: theme.spacing(1.5),
+    marginBottom: theme.spacing(1.75),
+    "& .MuiFormControlLabel-label": {
+      color: theme.palette.text.secondary,
+    },
+  })
+);
 
 const Register = () => {
   // ** States
-  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   // ** Hooks
-  const theme = useTheme()
-  const { settings } = useSettings()
-  const hidden = useMediaQuery(theme.breakpoints.down('md'))
+  const theme = useTheme();
+  const { settings } = useSettings();
+  const hidden = useMediaQuery(theme.breakpoints.down("md"));
 
   // ** Vars
-  const { skin } = settings
+  const { skin } = settings;
 
-  const imageSource = skin === 'bordered' ? 'auth-v2-register-illustration-bordered' : 'auth-v2-register-illustration'
+  const imageSource =
+    skin === "bordered"
+      ? "auth-v2-register-illustration-bordered"
+      : "auth-v2-register-illustration";
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    organization: "",
+    agreeToTerms: false,
+  });
 
+  // ** Router instance
+  const router = useRouter();
+
+  // ** Handle form submission
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // Perform form validation
+    if (
+      formData.username.trim() === "" ||
+      formData.email.trim() === "" ||
+      formData.password.trim() === "" ||
+      formData.organization.trim() === "" ||
+      !formData.agreeToTerms
+    ) {
+      alert(
+        "Please fill in all the fields and agree to the Terms and Conditions."
+      );
+      return; // Exit early if the form is not valid
+    }
+
+    console.log("Form Data:", formData); //  Console the form data
+    //  Redirect to login page
+    router.push("/login");
+  };
   return (
-    <Box className='content-right' sx={{ backgroundColor: 'background.paper' }}>
+    <Box className="content-right" sx={{ backgroundColor: "background.paper" }}>
       {!hidden ? (
         <Box
           sx={{
             flex: 1,
-            display: 'flex',
-            position: 'relative',
-            alignItems: 'center',
-            borderRadius: '20px',
-            justifyContent: 'center',
-            backgroundColor: 'customColors.bodyBg',
-            margin: theme => theme.spacing(8, 0, 8, 8)
+            display: "flex",
+            position: "relative",
+            alignItems: "center",
+            borderRadius: "20px",
+            justifyContent: "center",
+            backgroundColor: "customColors.bodyBg",
+            margin: (theme) => theme.spacing(8, 0, 8, 8),
           }}
         >
           <RegisterIllustration
-            alt='register-illustration'
+            alt="register-illustration"
             src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
           />
           <FooterIllustrationsV2 />
@@ -111,62 +152,142 @@ const Register = () => {
         <Box
           sx={{
             p: [6, 12],
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Box sx={{ width: '100%', maxWidth: 400 }}>
-            <img src='../images/logo.png' alt='logo' width='90' height='90' />
+          <Box sx={{ width: "100%", maxWidth: 400 }}>
+            <img src="../images/logo.png" alt="logo" width="90" height="90" />
             <Box sx={{ my: 1.5 }}>
-              <Typography variant='h3' sx={{ mb: 1.5 }}>
+              <Typography variant="h3" sx={{ mb: 1.5 }}>
                 Adventure starts here 🚀
               </Typography>
-              <Typography sx={{ color: 'text.secondary' }}>Make your app management easy and secure!</Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Make your app management easy and secure!
+              </Typography>
             </Box>
-            <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-              <CustomTextField autoFocus fullWidth sx={{ mb: 4 }} label='Username' placeholder='johndoe' />
-              <CustomTextField fullWidth label='Email' sx={{ mb: 4 }} placeholder='user@email.com' />
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+              <CustomTextField
+                autoFocus
+                fullWidth
+                sx={{ mb: 4 }}
+                label="Username"
+                placeholder="johndoe"
+                value={formData.username}
+                onChange={(e) =>
+                  setFormData({ ...formData, username: e.target.value })
+                }
+              />
               <CustomTextField
                 fullWidth
-                label='Password'
+                label="Email"
                 sx={{ mb: 4 }}
-                id='auth-login-v2-password'
-                type={showPassword ? 'text' : 'password'}
+                placeholder="user@email.com"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+              <CustomTextField
+                fullWidth
+                label="Password"
+                sx={{ mb: 4 }}
+                id="auth-login-v2-password"
+                type={showPassword ? "text" : "password"}
                 InputProps={{
                   endAdornment: (
-                    <InputAdornment position='end'>
+                    <InputAdornment position="end">
                       <IconButton
-                        edge='end'
-                        onMouseDown={e => e.preventDefault()}
+                        edge="end"
+                        onMouseDown={(e) => e.preventDefault()}
                         onClick={() => setShowPassword(!showPassword)}
                       >
-                        <Icon fontSize='1.25rem' icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} />
+                        <Icon
+                          fontSize="1.25rem"
+                          icon={showPassword ? "tabler:eye" : "tabler:eye-off"}
+                        />
                       </IconButton>
                     </InputAdornment>
-                  )
+                  ),
                 }}
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
               />
-              <CustomTextField fullWidth label='Organization' placeholder='Initializ Inc.' />
+              <CustomTextField
+                fullWidth
+                label="Organization"
+                placeholder="Initializ Inc."
+                value={formData.organization}
+                onChange={(e) =>
+                  setFormData({ ...formData, organization: e.target.value })
+                }
+              />
               <FormControlLabel
-                control={<Checkbox />}
-                sx={{ mb: 4, mt: 1.5, '& .MuiFormControlLabel-label': { fontSize: theme.typography.body2.fontSize } }}
+                control={
+                  <Checkbox
+                    checked={formData.agreeToTerms}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        agreeToTerms: e.target.checked,
+                      })
+                    }
+                  />
+                }
+                sx={{
+                  mb: 4,
+                  mt: 1.5,
+                  "& .MuiFormControlLabel-label": {
+                    fontSize: theme.typography.body2.fontSize,
+                  },
+                }}
                 label={
-                  <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
-                    <Typography sx={{ color: 'text.secondary' }}>I agree to</Typography>
-                    <Typography component={LinkStyled} href='/' onClick={e => e.preventDefault()} sx={{ ml: 1 }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography sx={{ color: "text.secondary" }}>
+                      I agree to
+                    </Typography>
+                    <Typography
+                      component={LinkStyled}
+                      href="/"
+                      onClick={(e) => e.preventDefault()}
+                      sx={{ ml: 1 }}
+                    >
                       privacy policy & terms
                     </Typography>
                   </Box>
                 }
               />
-              <Button fullWidth type='submit' variant='contained' sx={{ mb: 4 }}>
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mb: 4 }}
+              >
                 Sign up
               </Button>
-              <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
-                <Typography sx={{ color: 'text.secondary', mr: 2 }}>Already have an account?</Typography>
-                <Typography component={LinkStyled} href='/login'>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography sx={{ color: "text.secondary", mr: 2 }}>
+                  Already have an account?
+                </Typography>
+                <Typography component={LinkStyled} href="/login">
                   Sign in instead
                 </Typography>
               </Box>
@@ -175,11 +296,11 @@ const Register = () => {
         </Box>
       </RightWrapper>
     </Box>
-  )
-}
+  );
+};
 
-Register.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+Register.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>;
 
-Register.guestGuard = true
+Register.guestGuard = true;
 
-export default Register
+export default Register;
