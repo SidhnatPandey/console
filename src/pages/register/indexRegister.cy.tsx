5c1287c698bef41994601720995be70c796adacd
sachinsshakya <sachinagra.shakya@gmail.com>
2023-09-01 14:12:21 +0530

    Deleted test file created for the component testing of registration position

--- src/pages/register/indexRegister.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Register from './index'
-
-describe('<Register />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Register />)
-  })
-})
