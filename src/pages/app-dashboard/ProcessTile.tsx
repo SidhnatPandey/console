122440bc391de55582d51de9e20eb094f9fa67f1
sachinsshakya <sachinagra.shakya@gmail.com>
2023-09-05 21:19:29 +0530

    INIT-71 * Updated few styling

--- src/pages/app-dashboard/ProcessTile.tsx
+++ src/pages/app-dashboard/ProcessTile.tsx
@@ -4,9 +4,9 @@ import GridViewIcon from '@mui/icons-material/GridView';
 const ProcessTile = () => {
     return (
         <>
-            <Card sx={{ width: '150px', height: '150px', margin: '20px', borderRadius: '30px', textAlign: 'center', paddingTop: '40px' }}>
-                <GridViewIcon />
-                <h3>Clone</h3>
+            <Card sx={{ minWidth: '120px', height: '120px', margin: '20px', borderRadius: '30px', textAlign: 'center', padding: '30px 10px 10px 10px' }}>
+                <GridViewIcon fontSize="large" />
+                <h3 className="mt-0">Clone</h3>
             </Card>
         </>
     )
