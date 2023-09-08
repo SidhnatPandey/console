0d8684c96e0fca152725e9a5266115ce6bd2bfb3
ShivaniShekhawat26 <136352208+ShivaniShekhawat26@users.noreply.github.com>
2023-09-08 17:08:55 +0530

    feature/INIT-71-App-Dashboard *processTile file updations. *global file updated as gave scroll container some css.

--- src/pages/app-dashboard/ProcessTile.tsx
+++ src/pages/app-dashboard/ProcessTile.tsx
@@ -5,14 +5,14 @@ import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 interface ProcessTileProps {
   title: string;
-  isApproval?: boolean;
+  status: "completed" | "running" | "pending";
   onClick: () => void;
   isSelected: boolean;
 }
 
 const ProcessTile: React.FC<ProcessTileProps> = ({
   title,
-  isApproval = false,
+  status,
   onClick,
   isSelected,
 }) => {
@@ -28,10 +28,11 @@ const ProcessTile: React.FC<ProcessTileProps> = ({
         padding: "30px 10px 10px 10px",
         border: isSelected ? "2px solid rgb(115, 83, 229)" : "2px solid transparent",
         cursor: "pointer",
+        
       }}
     >
-      {isApproval ? (
-        <GridViewIcon fontSize="large" color="primary" /> // Use GridViewIcon for Approval
+      {status === "running" ? (
+        <GridViewIcon fontSize="large" color="primary" />
       ) : (
         <GridViewIcon fontSize="large" />
       )}
@@ -45,90 +46,41 @@ const ProcessTile: React.FC<ProcessTileProps> = ({
 const AppCreationFlow: React.FC = () => {
   const [selectedTile, setSelectedTile] = useState<string | null>(null);
 
+  const processList: { title: string; status: "completed" | "running" | "pending" }[] = [
+    { title: "Clone", status: "completed" },
+    { title: "Build", status: "completed" },
+    { title: "Package", status: "completed" },
+    { title: "SCA", status: "completed" },
+    { title: "Scan", status: "completed" },
+    { title: "Approval 1", status: "running" },
+    { title: "Stg", status: "completed" },
+    { title: "Approval 2", status: "running" },
+    { title: "Prod", status: "completed" },
+  ];
+
   const handleTileClick = (title: string) => {
     setSelectedTile(title === selectedTile ? null : title);
   };
 
   return (
     <div
-      style={{
-        overflowX: "auto",
-        display: "flex",
-        alignItems: "center",
-        padding: "20px",
-      }}
+    className={`scroll-container`}
     >
-      <ProcessTile
-        title="Clone"
-        onClick={() => handleTileClick("Clone")}
-        isSelected={selectedTile === "Clone"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Build"
-        onClick={() => handleTileClick("Build")}
-        isSelected={selectedTile === "Build"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Package"
-        onClick={() => handleTileClick("Package")}
-        isSelected={selectedTile === "Package"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="SCA"
-        onClick={() => handleTileClick("SCA")}
-        isSelected={selectedTile === "SCA"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Scan"
-        onClick={() => handleTileClick("Scan")}
-        isSelected={selectedTile === "Scan"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Approval" // Give a unique title
-        isApproval
-        onClick={() => handleTileClick("Approval 1")}
-        isSelected={selectedTile === "Approval 1"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Stg"
-        onClick={() => handleTileClick("Stg")}
-        isSelected={selectedTile === "Stg"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Approval" // Give a unique title
-        isApproval
-        onClick={() => handleTileClick("Approval 2")}
-        isSelected={selectedTile === "Approval 2"}
-      />
-
-      <ArrowRightAltIcon sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }} />
-
-      <ProcessTile
-        title="Prod"
-        onClick={() => handleTileClick("Prod")}
-        isSelected={selectedTile === "Prod"}
-      />
+      {processList.map((process, index) => (
+        <React.Fragment key={index}>
+          <ProcessTile
+            title={process.title}
+            status={process.status}
+            onClick={() => handleTileClick(process.title)}
+            isSelected={selectedTile === process.title}
+          />
+          {index < processList.length - 1 && (
+            <ArrowRightAltIcon
+              sx={{ fontSize: "60px", color: "rgb(115, 83, 229)" }}
+            />
+          )}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
