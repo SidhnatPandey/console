4e2155693d25b01e3d03b43bdde0727dd0bcd5f2
sachinsshakya <sachinagra.shakya@gmail.com>
2023-10-13 20:51:57 +0530

    INIT-124: Vertical tabs moved to logs screen

--- src/pages/apps/app-dashboard/ProcessDetails.tsx
+++ src/pages/apps/app-dashboard/ProcessDetails.tsx
@@ -1,19 +1,7 @@
 import React, { SyntheticEvent, useEffect, useState } from "react";
 import { Grid, Card } from "@mui/material";
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import TabList from "@mui/lab/TabList";
-import TabContext from "@mui/lab/TabContext";
 import Typography from "@mui/material/Typography";
 import ProcessLogs from "./ProcessLogs";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import PendingIcon from "@mui/icons-material/Pending";
-import LoopIcon from "@mui/icons-material/Loop";
-import CustomAvatar from "src/@core/components/mui/avatar";
-import Icon from "src/@core/components/icon";
-import { Container } from "@mui/system";
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-
 
 // Define the prop types for the ProcessDetails component
 interface ProcessDetailsProps {
@@ -41,120 +29,10 @@ interface Step {
 const ProcessDetails: React.FC<ProcessDetailsProps> = ({
   supplyChainStepData,
 }) => {
-  const [value, setValue] = useState<string>("0");
-  const [step, setStep] = useState<Step>(supplyChainStepData?.steps[0]);
-
-  const handleChange = (event: SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-  
-
-const icon = (status: string) => {
-  
-  switch (status) {
-    case "Succeeded":
-      return (
-        <CustomAvatar
-          skin="light"
-          color={"success"}
-          sx={{
-            marginTop: 1,
-            width: 20,
-            height: 20,
-            display: "flex",
-            alignItems: "center",
-          }}
-          style={{ marginRight: 10 }}
-        >
-          <Icon icon={"ph:check-light"} />
-        </CustomAvatar>
-      );
-    case "InProgress":
-      return (
-        <>
-         
-            <LoopIcon
-              style={{ animation: "spin 4s linear infinite", marginLeft:"5px" }}
-            
-              color="primary"
-              fontSize="medium"
-            />
-            <style>
-              {`
-              @keyframes spin {
-                0% { transform: rotate(360deg); }
-                100% { transform: rotate(0deg); }
-              }`}
-            </style>
-        
-        </>
-      );
-    case "Waiting":
-      return (
-        <PendingIcon fontSize="medium" style={{ color: "rgb(85, 85, 85)" }} />
-      );
-    case "Failed":
-      return <ErrorOutlineIcon  fontSize="medium" style={{ color: "red" }} />;
-    default:
-      return (
-        <HelpOutlineIcon fontSize="medium" style={{ color: "rgb(85, 85, 85)" }} />
-      );
-  }
-};
-
-
-  useEffect(() => {
-    setValue("0");
-    if (supplyChainStepData) {
-      setStep(supplyChainStepData.steps[0]);
-    }
-  }, [supplyChainStepData?.steps[0]]);
-
-  const handleTabChange = (step: Step, index: number) => {
-    setStep(step);
-  };
 
   return (
     <>
       <Card sx={{ display: "flex", flexDirection: "row" }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center", // Center horizontally
-            alignItems: "center", // Center vertically
-            width: "20%", // Adjust the width as needed
-            padding: "20px", // Add padding for spacing
-          }}
-        >
-          <TabContext value={value}>
-            <TabList
-              orientation="vertical"
-              onChange={handleChange}
-              aria-label="vertical tabs example"
-              sx={{
-                width: "100%", // Make the tabs take the full width of the Box
-                "& .MuiTab-root": {
-                  fontSize: "1.2rem", // Increase the font size
-                  padding: "10px 20px", // Adjust padding for larger size
-                },
-              }}
-            >
-              {supplyChainStepData?.steps.map((step, index) => {
-                return (
-                  <Tab
-                    iconPosition="end"
-                    value={index.toString()}
-                    label={step.run_name}
-                    key={index}
-                    onClick={() => handleTabChange(step, index)}
-                    icon={icon(step.status)}
-                  />
-                );
-              })}
-            </TabList>
-          </TabContext>
-        </Box>
-
         <Grid container spacing={2} style={{ padding: "30px" }}>
           <Grid item xs={12}>
             <Typography variant="h5">
@@ -197,7 +75,7 @@ const icon = (status: string) => {
         </Grid>
       </Card>
       <br></br>
-      <ProcessLogs log={step?.log} />
+      <ProcessLogs steps={supplyChainStepData.steps} />
     </>
   );
 };
