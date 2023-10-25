659d00d3a71b15ba7249ffed13bd7c159beb7112
MK <mk@initializ.io>
2023-10-24 20:04:56 -0400

    added config to be loaded at runtime using os env

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+  env: {
+    GITHUB_APP_NAME: process.env.GITHUB_APP_NAME,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_APP_DASHBOARD_REFRESH_TIMER: process.env.NEXT_PUBLIC_APP_DASHBOARD_REFRESH_TIMER,
+    NEXT_PUBLIC_GITHUB_URL: process.env.NEXT_PUBLIC_GITHUB_URL
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
