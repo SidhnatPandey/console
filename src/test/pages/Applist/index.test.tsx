e78e090047132f3c476702d8f48e493689307236
Jagpreet.Bhatia <Jagpreet.Bhatia@sandata.com>
2023-11-01 19:19:30 +0530

    Init-136: Applist unittest cases minor chaanges

--- src/test/pages/Applist/index.test.tsx
+++ src/test/pages/Applist/index.test.tsx
@@ -1,17 +1,17 @@
 // test('pagination works as expected', async () => {
 //     const { findByLabelText, findByText } = render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
-    
+
 //     // Use findBy instead of getBy for asynchronous queries
 //     const rowsPerPageDropdown = await findByLabelText('Rows per page:');
 //     fireEvent.change(rowsPerPageDropdown, { target: { value: '10' } });
-  
+
 //     // Use findBy instead of getBy for asynchronous queries
 //     const nextPageButton = await findByText('Next');
 //     fireEvent.click(nextPageButton);
-  
+
 //     // Add your assertions here, for example, check if the next page is displayed
 //   });
-  
+
 
 import React from 'react';
 import { fireEvent, render, waitFor } from '@testing-library/react';
@@ -35,22 +35,23 @@ jest.mock('src/services/appService', () => ({
   ),
 }));
 
-it('renders Apps component without error',() => {
+it('renders Apps component without error', () => {
   render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
 });
 
 test('sorts table when column header is clicked', async () => {
- render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
-    const columnHeader = await screen.getByText('NAME');
-    fireEvent.click(columnHeader);
+  render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
+  const columnHeader = screen.getByText('NAME');
+  fireEvent.click(columnHeader);
+});
+
+test('pagination works as expected', async () => {
+  render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
+  waitFor(() => {
+    screen.findByLabelText('Rows per page:', {}, { timeout: 5000 });
+    screen.findByText('Next', {}, { timeout: 5000 });
   });
 
-  test('pagination works as expected', async () => {
-    render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
-     waitFor(async () => {
-      await screen.findByLabelText('Rows per page:', {}, { timeout: 5000 }); 
-      await screen.findByText('Next', {}, { timeout: 5000 });
-    });
-  });
-  
+});
+
 
