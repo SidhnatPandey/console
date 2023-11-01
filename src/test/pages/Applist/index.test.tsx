3969b2d00dd264b52e012db04ef9ef946b68ea6a
Jagpreet.Bhatia <Jagpreet.Bhatia@sandata.com>
2023-11-01 19:55:03 +0530

    Init-136: Applist unittest cases minor chaanges

--- src/test/pages/Applist/index.test.tsx
+++ src/test/pages/Applist/index.test.tsx
@@ -47,11 +47,14 @@ test('sorts table when column header is clicked', async () => {
 
 test('pagination works as expected', async () => {
   render(<Apps selectedRow={null} setSelectedRow={jest.fn()} />);
-  waitFor(async() => {
-   await screen.findByLabelText('Rows per page:', {}, { timeout: 5000 });
-    await screen.findByText('Next', {}, { timeout: 5000 });
-  });
 
+  await waitFor(
+    () => {
+      expect(screen.getByLabelText('Rows per page:')).toBeInTheDocument();
+      expect(screen.getByLabelText('Go to next page')).toBeInTheDocument();
+    },
+    { timeout: 10000 }
+  );
 });
 
 
