ed42dfae3892a02d1c02c9af1a69af0cab95d038
MK <52723052+initializ-mk@users.noreply.github.com>
2023-11-04 15:19:22 -0400

    Update interceptor.ts

--- src/@core/services/interceptor.ts
+++ src/@core/services/interceptor.ts
@@ -40,6 +40,9 @@ axiosInstance.interceptors.response.use(
                 localStorage.clear();
                 window.location.href = '/login';
             }
+            if (error?.response.status === 404) {
+               return error.response;
+            }
         }
         return Promise.reject(error);
     }
