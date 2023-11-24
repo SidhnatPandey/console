d53a4d465cc851f97ec33b340eaa5572e22e997b
Sachin Singh <ssingh@initializ.io>
2023-11-24 21:17:35 +0530

    Introduced logic to changes the base URL based on the microservice

--- src/@core/services/interceptor.ts
+++ src/@core/services/interceptor.ts
@@ -9,6 +9,18 @@ const axiosInstance = axios.create({
     baseURL: baseUrl, // Your API base URL
 });
 
+// Changes the base url for the different Services
+export const setApiBaseUrl = (service: string = 'core') => {
+    let base_url = baseUrl;
+    switch (service) {
+        case 'core':
+            base_url = baseUrl;
+        case 'security':
+            base_url = env('NEXT_PUBLIC_SECURITY_BASE_URL');
+    }
+    axiosInstance.defaults.baseURL = base_url;
+};
+
 // Request Interceptor: Modify or add headers to outgoing requests
 axiosInstance.interceptors.request.use(
     (config) => {
--- src/services/appService.ts
+++ src/services/appService.ts
@@ -1,25 +1,31 @@
 import { APP_API } from "src/@core/static/api.constant";
 import { get, post } from "../@core/services/masterServices";
+import { setApiBaseUrl } from "src/@core/services/interceptor";
 
 export const appNameExists = (appName: any) => {
+  setApiBaseUrl();
   const url = APP_API.checkAppNameExists.replace("{appName}", appName);
   return get(url).then((response) => response?.data);
 };
 export const sendCode = (code: string) => {
+  setApiBaseUrl();
   const url = APP_API.sendGitCode.replace("{code}", code);
   return get(url).then((response) => response?.data);
 };
 
 export const getGitOwner = () => {
+  setApiBaseUrl();
   return get(APP_API.gitOwner).then((response) => response?.data);
 };
 
 export const getRepositories = (gituser: string) => {
+  setApiBaseUrl();
   const url = APP_API.getRepositories.replace("{gituser}", gituser);
   return get(url).then((response) => response?.data);
 };
 
 export const getBranch = (repo: string, gituser: string) => {
+  setApiBaseUrl();
   const arr = repo.split("/");
   let url = APP_API.getBranches.replace("{repoOwner}", arr[0]);
   url = url.replace("{repoName}", arr[1]);
@@ -28,17 +34,20 @@ export const getBranch = (repo: string, gituser: string) => {
 };
 
 export const saveApp = (app: any) => {
+  setApiBaseUrl();
   return post(APP_API.saveApp, app).then(
     (response) => response?.data
   )
 }
 
 export const appList = () => {
+  setApiBaseUrl();
   const url = APP_API.appList;
   return get(url).then((response) => response?.data);
 };
 
 export const appDetails = (id: any) => {
+  setApiBaseUrl();
   let url = APP_API.appDetails;
   url = url.replace("{appId}", id);
   return get(url).then((response) => response?.data);
