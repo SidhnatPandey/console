1ee82128d8caeca68c0b6994d646df5ae5c0e549
Jagpreet.Bhatia <Jagpreet.Bhatia@sandata.com>
2024-01-02 16:36:36 +0530

    bug-fixes *Updated the app-logs testcases

--- src/test/pages/app-logs/app-logs.test.tsx
+++ src/test/pages/app-logs/app-logs.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen, findAllByText } from "@testing-library/react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import AppLogs from "src/pages/apps/app-dashboard/AppLogs";
 import "@testing-library/jest-dom";
 
@@ -11,50 +11,50 @@ jest.mock("src/services/dashboardService", () => ({
 
 describe('<AppLogs />', () => {
   it('renders the component', () => {
-    const { getByTestId } = render(<AppLogs appId="123" />);
-    expect(getByTestId('app-logs-card')).toBeInTheDocument();
+    render(<AppLogs appId="123" />);
+    expect(screen.getByTestId('app-logs-card')).toBeInTheDocument();
   });
 });
 
-it(' searchbox showing correctly', async () => {
-  const { getByTestId } = render(<AppLogs appId="123" />);
-  expect(getByTestId('environments-title')).toBeInTheDocument();
+it('searchbox showing correctly', async () => {
+  render(<AppLogs appId="123" />);
+  expect(screen.getByTestId('environments-title')).toBeInTheDocument();
 });
 
 it('changes tabs correctly', () => {
-  const { getByText, getByTestId } = render(<AppLogs appId="123" />);
-  const prodTab = getByText('Prod');
+  render(<AppLogs appId="123" />);
+  const prodTab = screen.getByText('Prod');
   fireEvent.click(prodTab);
-  expect(getByTestId('logs-title')).toHaveTextContent('Prod Logs');
+  expect(screen.getByTestId('logs-title')).toHaveTextContent('Prod Logs');
 });
 
 it('changes tabs correctly', () => {
-  const { getByText, getByTestId } = render(<AppLogs appId="123" />);
-  const prodTab = getByText('Stg');
+  render(<AppLogs appId="123" />);
+  const prodTab = screen.getByText('Stg');
   fireEvent.click(prodTab);
-  expect(getByTestId('logs-title')).toHaveTextContent('Stg Logs');
+  expect(screen.getByTestId('logs-title')).toHaveTextContent('Stg Logs');
 });
 
 it('changes tabs correctly', () => {
-  const { getByText, getByTestId } = render(<AppLogs appId="123" />);
-  const prodTab = getByText('Test');
+  render(<AppLogs appId="123" />);
+  const prodTab = screen.getByText('Test');
   fireEvent.click(prodTab);
-  expect(getByTestId('logs-title')).toHaveTextContent('Test Logs');
+  expect(screen.getByTestId('logs-title')).toHaveTextContent('Test Logs');
 });
 
-it(' searchbox showing correctly', async () => {
-  const { getByTestId } = render(<AppLogs appId="123" />);
-  expect(getByTestId('search-field')).toBeInTheDocument();
+it('searchbox showing correctly', async () => {
+  render(<AppLogs appId="123" />);
+  expect(screen.getByTestId('search-field')).toBeInTheDocument();
 });
 
-it(' searchbox showing correctly', async () => {
-  const { getByTestId } = render(<AppLogs appId="123" />);
-  expect(getByTestId('tab-list-box')).toBeInTheDocument();
+it('searchbox showing correctly', async () => {
+  render(<AppLogs appId="123" />);
+  expect(screen.getByTestId('tab-list-box')).toBeInTheDocument();
 });
 
 it('scrolls to bottom on new logs', async () => {
-  const { getByTestId } = render(<AppLogs appId="123" />);
-  const logsContainer = getByTestId('logs-container');
+  render(<AppLogs appId="123" />);
+  const logsContainer = screen.getByTestId('logs-container');
 
   expect(logsContainer.scrollTop).toBe(logsContainer.scrollHeight - logsContainer.clientHeight);
 });
