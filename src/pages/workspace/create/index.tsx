bc58f3de9c1caeef0e0a763db481d4037999bbdb
ShivaniShekhawat26 <sshekhawat@initializ.io>
2024-01-02 19:46:30 +0530

    INIT-267 Unit test case done for Create workspace

--- src/pages/workspace/create/index.tsx
+++ src/pages/workspace/create/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Card, Grid, TextField, Button } from '@mui/material';
-import toast from 'react-hot-toast';
-import { workspace } from 'src/services/appService';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Card, Grid, TextField, Button } from "@mui/material";
+import toast from "react-hot-toast";
+import { workspace } from "src/services/appService";
 
 type FormData = {
   workspace_name: string;
@@ -21,21 +21,22 @@ const CreateWorkspace: React.FC = () => {
     workspace(data)
       .then((response) => {
         toast.success("Workspace Created Successfully");
-        reset(); 
+        reset();
       })
       .catch((error) => {
         toast.error(error.message || "An error occurred");
       });
   };
-  
+
   return (
-    <Card sx={{ p: 7 , pt:1}}>
+    <Card sx={{ p: 7, pt: 1 }}>
       <h2>Create New Workspace</h2>
       <p>Add Details To Create a New Workspace</p>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={3}>
           <Grid item xs={8}>
             <TextField
+              data-testid="workspaceName"
               fullWidth
               label="Workspace Name *"
               variant="outlined"
@@ -49,6 +50,7 @@ const CreateWorkspace: React.FC = () => {
 
           <Grid item xs={8}>
             <TextField
+              data-testid="workspaceDescription"
               fullWidth
               label="Workspace Description"
               variant="outlined"
@@ -58,8 +60,9 @@ const CreateWorkspace: React.FC = () => {
             />
           </Grid>
 
-          <Grid item xs={12} style={{ textAlign: 'left' }}>
+          <Grid item xs={12} style={{ textAlign: "left" }}>
             <Button
+              data-testid="Button"
               size="large"
               variant="contained"
               type="submit"
