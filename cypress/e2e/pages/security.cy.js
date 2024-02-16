52cfc3f91c07ebe80d8233b49eda28a8685f93aa
Jagpreet.Bhatia <Jagpreet.Bhatia@sandata.com>
2024-02-16 16:48:54 +0530

    Updated E2E testcases for security

--- cypress/e2e/pages/security.cy.js
+++ cypress/e2e/pages/security.cy.js
@@ -1,22 +1,36 @@
-it('Loads the login page', () => {
-    cy.visit('/login'); // Assuming the login page URL is '/login'
-    cy.contains(`Welcome to initializ! 👋🏻`); // Verify page content
-  });
-
-
-  it('To check the security screen ', () => {
-    cy.visit('/login');
-    cy.get('input[id=user-email-input]').type('lrawat@initializ.io');
-    cy.get('input[id=auth-login-v2-password]').type('Password@1');
-    cy.get('button[type="submit"]').click();
-    cy.url({ timeout: 30000 }).should('include', '/workspace/65a65ff87915132683c4c3ed/');
-    
-    cy.get('.css-fnrg6y-MuiButtonBase-root-MuiIconButton-root').click()
-    cy.get('.css-6bpq2m-MuiButtonBase-root-MuiListItemButton-root').click()
-    cy.get('.Mui-selected').click()
-    cy.get('.MuiList-root > :nth-child(3) > .MuiButtonBase-root').click()
-    cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1) > a').click()
-    cy.get('.MuiBox-root > .MuiInputBase-root > .MuiInputBase-input').type('DemoSec')
-
-    cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1) > a').click()
+it('To check the security screen while prod and current button is working fine. ', () => {
+  cy.visit('https://console.dev.initializ.ai/login/');
+  cy.get('input[id=user-email-input]').type('lrawat@initializ.io');
+  cy.get('input[id=auth-login-v2-password]').type('Password@1');
+  cy.get('button[type="submit"]').click();
+  cy.url({ timeout: 30000 }).should('include', '/workspace/65a65ff87915132683c4c3ed/');
+  cy.get('.css-hqz9my').click()
+  cy.get('.Mui-selected').click()
+  cy.get('.MuiList-root > :nth-child(3) > .MuiButtonBase-root').click()
+  cy.get('[value="prod"]').click()
+})
+ 
+it('To check the security screen whether search button functionality is workin ', () => {
+  cy.visit('https://console.dev.initializ.ai/login/');
+  cy.get('input[id=user-email-input]').type('lrawat@initializ.io');
+  cy.get('input[id=auth-login-v2-password]').type('Password@1');
+  cy.get('button[type="submit"]').click();
+  cy.url({ timeout: 30000 }).should('include', '/workspace/65a65ff87915132683c4c3ed/');
+  cy.get('.css-hqz9my').click()
+  cy.get('.Mui-selected').click()
+  cy.get('.MuiList-root > :nth-child(3) > .MuiButtonBase-root').click()
+  cy.get('.MuiBox-root > .MuiInputBase-root').click().type('DemoSec')
+})
+ 
+it('To check the security screen is redirecting succesully ', () => {
+  cy.visit('https://console.dev.initializ.ai/login/');
+  cy.get('input[id=user-email-input]').type('lrawat@initializ.io');
+  cy.get('input[id=auth-login-v2-password]').type('Password@1');
+  cy.get('button[type="submit"]').click();
+  cy.url({ timeout: 30000 }).should('include', '/workspace/65a65ff87915132683c4c3ed/');
+  cy.get('.css-hqz9my').click()
+  cy.get('.Mui-selected').click()
+  cy.get('.MuiList-root > :nth-child(3) > .MuiButtonBase-root').click()
+  cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1) > a').click()
+  cy.get(':nth-child(2) > :nth-child(1) > a').click()
 });
